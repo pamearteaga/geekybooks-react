f1c12b53996b292f7b45c35a2253322336d3d5ff
pamearteaga <pe_arteaga@hotmail.com>
2018-03-14 12:32:18 -0300

    Componentes

---
+++ src/componentes/contenido.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const contenido = document.getElementById('contenido');
+
+ReactDOM.render(<h1>Hola mundo</h1>,contenido);
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+const contenido = document.getElementById('contenido');
+
+ReactDOM.render(<h1>Hola mundo</h1>,contenido);
